e41deae9282d9fe2582a72cfa840f2b829277d16
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-04-29 11:33:52 -0600

    Add files via upload

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.shortcuts import render
 from . import views
 
 urlpatterns = [
     path('hello/',views.hello),
-    path('create/',views.create)
+    path('create/',views.create),
+    path('list/<ls>',views.list)
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import Createlist
+from .models import lists,question
 # Create your views here.
 # request -> response
 
@@ -8,8 +10,28 @@ def hello(request):
 
 
 def home (request):
-    return render(request, 'main/home.html')
+    return render(request, 'main/home.html',{})
 
 #creates the form that will be used to
 def create(request):
-    return (render(request),'main/create.html')
+    if request.method=="POST":
+        form = Createlist(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            t = lists(name=name)
+            t.save()
+
+    else:
+        questionare = Createlist()
+    return render(request,'main/create.html',{"form":questionare})
+
+def list(request,ls):
+    category = lists.objects.get(name=ls)
+    return render(request,"main/list.html",{"ls":category})
+
+#add items to the list
+def add(request,name):
+    if request.method == "POST":
+        txt = request.POST.get("save")
+
+
