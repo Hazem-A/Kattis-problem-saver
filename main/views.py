45194c16fa370b70db69e33c7b8c56b8a0de8404
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-04-29 14:04:36 -0600

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -4,7 +4,6 @@ from .forms import Createlist
 from .models import lists,question
 # Create your views here.
 # request -> response
-
 def hello(request):
     return render(request, 'main/questions.html', {'name': 'Kattis'})
 
@@ -15,23 +14,24 @@ def home (request):
 #creates the form that will be used to
 def create(request):
     if request.method=="POST":
-        form = Createlist(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data["name"]
-            t = lists(name=name)
+        questionare = Createlist(request.POST)
+        if questionare.is_valid():
+            name = questionare.cleaned_data["name"]
+            t = lists.objects.create(name=name)
             t.save()
-
+            return render(request, 'main/create.html', {"form": questionare})
     else:
         questionare = Createlist()
     return render(request,'main/create.html',{"form":questionare})
 
 def list(request,ls):
     category = lists.objects.get(name=ls)
+    if request.method=="POST":
+        txt = request.POST.get("add")
+
+        category.question_set.create(Kattis_id=txt,link="https://open.kattis.com/problems/"+txt)
+
     return render(request,"main/list.html",{"ls":category})
 
-#add items to the list
-def add(request,name):
-    if request.method == "POST":
-        txt = request.POST.get("save")
 
 
